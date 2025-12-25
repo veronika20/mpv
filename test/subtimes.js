306a1ea147e2d1c6f500665e9f8dedf428845067
veronika20 <veronika20@users.noreply.github.com>
2025-12-25 19:19:00 +0000

    chore: activity update

--- test/subtimes.js
+++
@@ -1,7 +0,0 @@
-function subtimes() {
-  mp.msg.info("sub-start: " + mp.get_property_number("sub-start"));
-  mp.msg.info("sub-end: " + mp.get_property_number("sub-end"));
-  mp.msg.info("sub-text: " + mp.get_property_native("sub-text"));
-}
-
-mp.add_key_binding("t", "subtimes", subtimes);
